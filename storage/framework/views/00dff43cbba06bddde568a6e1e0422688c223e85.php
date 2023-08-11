636f89db4d948c7fdb56dc325fa0ae9d44485a6f
fahd shaykh <fahdshaykh@outlook.com>
2023-08-11 16:10:41 +0500

    basic setup

---
+++ storage/framework/views/00dff43cbba06bddde568a6e1e0422688c223e85.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- index.html  21 Nov 2019 03:44:50 GMT -->
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title><?php echo $__env->yieldContent('title'); ?></title>
+  <link rel="stylesheet" href="<?php echo e(asset('dashboard')); ?>/assets/css/app.min.css">
+  <link rel="stylesheet" href="<?php echo e(asset('dashboard')); ?>/assets/bundles/datatables/datatables.min.css">
+  <link rel="stylesheet" href="<?php echo e(asset('dashboard')); ?>/assets/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo e(asset('dashboard')); ?>/assets/bundles/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="<?php echo e(asset('dashboard')); ?>/assets/bundles/jquery-selectric/selectric.css">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="<?php echo e(asset('dashboard')); ?>/assets/css/style.css">
+  <link rel="stylesheet" href="<?php echo e(asset('dashboard')); ?>/assets/css/components.css">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="<?php echo e(asset('dashboard')); ?>/assets/css/custom.css">
+  <link rel='shortcut icon' type='image/x-icon' href="<?php echo e(asset('dashboard')); ?>/assets/img/favicon.ico" />
+</head>
+
+<body><?php /**PATH D:\work\works OutSource\Salva_Fast_Food\resources\views/dashboard/includes/head.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/a4d2a8d731f1d60b376b573a04fd06055904ba53.php
@@ -0,0 +1,32 @@
+<div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="/admin"> <img alt="image" src="<?php echo e(asset('dashboard')); ?>/assets/img/logo.png" class="header-logo" /> <span
+                class="logo-name">Salva</span>
+            </a>
+          </div>
+          <ul class="sidebar-menu">
+            <!-- <li class="menu-header">Main</li> -->
+            <li class="dropdown active">
+              <a href="/admin" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i
+                  data-feather="briefcase"></i><span>Careers</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="<?php echo e(route('jobs')); ?>">Jobs</a></li>
+                <li><a class="nav-link" href="widget-data.html">Applications</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="mail"></i><span>Queries</span></a>
+              <ul class="dropdown-menu">
+                <li><a class="nav-link" href="email-inbox.html">Contact Us</a></li>
+                <li><a class="nav-link" href="email-compose.html">Franchise</a></li>
+               </ul>
+            </li>            
+            <li><a class="nav-link" href="blank.html"><i data-feather="settings"></i><span>Settings </span></a></li>
+
+          </ul>
+        </aside>
+      </div><?php /**PATH D:\work\works OutSource\Salva_Fast_Food\resources\views/dashboard/includes/sidebar.blade.php ENDPATH**/ ?>
